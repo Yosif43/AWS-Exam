753fc4b51bd8a218fe3f67c090c05747fbf4fafb
Yosif43 <yosifyosifov4393@GMAIL.COM>
2025-08-03 08:56:01 +0100

    .

--- test/aws-exam.test.ts
+++ test/aws-exam.test.ts
@@ -1,17 +1,12 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsExam from '../lib/aws-exam-stack';
+import {Template} from "aws-cdk-lib/assertions";
+import {App} from "aws-cdk-lib";
+import {AwsExamStack} from "../lib/aws-exam-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-exam-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsExam.AwsExamStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+   const app = new App();
+   const stack = new AwsExamStack(app, 'MyTestStack');
+   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+   expect(template).toMatchSnapshot();
+
 });
